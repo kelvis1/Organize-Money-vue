82d1a2d46bd3a92eed78a888d7e6c54bcf88ee9c
Kelvis Rodrigues <wive.rod20@gmail.com>
2024-05-14 21:01:09 -0300

    modifiquei a barra lateral com bootstrap e alterei algum nomes na propria

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,48 @@
 <template>
-  <div class="corFundo">
-    <div class="header">
-      <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu" />
-      <label for="openSidebarMenu" class="sidebarIconToggle">
-        <div class="spinner diagonal part-1"></div>
-        <div class="spinner horizontal"></div>
-        <div class="spinner diagonal part-2"></div>
-      </label>
-      <div id="sidebarMenu">
-        <ul class="sidebarMenuInner">
-          <li>Kelvis dev <span>Menu</span></li>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Costumer</a></li>
-          <li><a href="#">Estatisticas</a></li>
-          <li><a href="#">Outros</a></li>
-        </ul>
+ <nav class="navbar bg-success shadow">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Menu</span>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-fullscreen">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Menu</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      
+        <ul class="list-group list-group-flush">
+  <li class="list-group-item">HOME</li>
+  <li class="list-group-item">COSTUMIZAÇÃO</li>
+  <li class="list-group-item">ESTATISTICAS</li>
+  <li class="list-group-item">CONFIGURAÇÃO</li>
+  <li class="list-group-item">SOBRE</li>
+</ul>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-sucess" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
-    <div class="row mt-4 m-0 justify-content-center">
+  </div>
+</div>
+
+ <div class="row mt-4 m-0 justify-content-center">
       <Cards :titulo="'Saldo'"/>
       <Cards :titulo="'Despesa'"/>
       <Cards :titulo="'Outros'"/>
       <Boxcenter :titulo="'Boxcenter'" />
     </div>
-   
-  </div>
-  
+
+
 </template>
 
 <script>
@@ -38,135 +55,40 @@ export default {
   components: {
     Cards,
     Boxcenter,
-  },
-  data(){
-   return{
-     isSidebarOpen: false,
-   };
-  },
-
+  }
 };
 
 </script>
 
 <style scoped>
+
+.modal{
+  width: 300px;
+}
+
+.modal-content{
+  width: 300px;
+}
+.list-group-item{
+  background: #00ff88;
+}
+
+.list-group-item:hover{
+ background:  #77ffc0;
+}
+
+.modal-header{
+  background: linear-gradient(180deg,  #77ffc0 0%, #00ff88 100%);
+}
+
+.modal-body{
+  background: #00ff88;
+}
+
+.modal-footer{
+   background: linear-gradient(180deg,  #00ff88 0%,  #77ffc0 100%);
+}
+
 @import url("https://fonts.googleapis.com/css?family=Varela+Round");
 
-
-.corFundo {
-  background-color: #110932;
-
-}
-
-.header {
-  display: block;
-  margin: 0 auto;
-  width: 100%;
-  max-width: 100%;
-  box-shadow: none;
-  background-color: #201b2c;
-  position: relative;
-  height: 60px !important;
-  overflow: hidden;
-  z-index: 10;
-}
-
-.main {
-  margin: 0 auto;
-  display: block;
-  height: 100%;
-  margin-top: 60px;
-}
-
-.mainInner {
-  display: table;
-  height: 100%;
-  width: 100%;
-  text-align: center;
-}
-
-.mainInner div {
-  display: table-cell;
-  vertical-align: middle;
-  font-size: 3em;
-  font-weight: bold;
-  letter-spacing: 1.25px;
-}
-
-#sidebarMenu {
-  height: 100%;
-  position: fixed;
-  left: 0;
-  width: 250px;
-  margin-top: 60px;
-  transform: translateX(-250px);
-  transition: transform 250ms ease-in-out;
-  background: linear-gradient(180deg,  #00000056 50%, #514869 100%);
-}
-
-.sidebarMenuInner {
-  margin: 0;
-  padding: 0;
-  border-top: 1px solid rgba(255, 255, 255, 0.11);
-}
-
-.sidebarMenuInner li {
-  list-style: none;
-  color: #77ffc0;
-  text-transform: uppercase;
-  font-weight: bold;
-  padding: 20px;
-  cursor: pointer;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-}
-
-.sidebarMenuInner li span {
-  display: block;
-  font-size: 14px;
-  color: rgba(255, 255, 255, 0.5);
-}
-
-.sidebarMenuInner li a {
-  color: #77ffc0;
-  text-transform: uppercase;
-  font-weight: bold;
-  cursor: pointer;
-  text-decoration: none;
-}
-
-
-
-input[type="checkbox"]:checked ~ #sidebarMenu {
-  transform: translateX(0);
-}
-
-input[type="checkbox"] {
-  transition: all 0.3s;
-  box-sizing: border-box;
-  display: none;
-}
-
-.sidebarIconToggle {
-  transition: all 0.3s;
-  box-sizing: border-box;
-  cursor: pointer;
-  position: absolute;
-  z-index: 99;
-  height: 100%;
-  width: 100%;
-  top: 22px;
-  left: 15px;
-  height: 22px;
-  width: 22px;
-}
-
-.spinner {
-  transition: all 0.3s;
-  box-sizing: border-box;
-  position: relative;
-  height: 3px;
-  width: 100%;
-  background-color: #fff;
-  margin: 2px 0;
-}
 </style>
